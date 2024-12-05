21db2267e78eaf59decefc2f4cfc199bfe4de176
AntonioDeFelipeVargas <antoniodefelipee@gmail.com>
2024-12-05 12:39:47 -0600

    Limpieza para cambio de proyecto

--- src/main/java/com/wanderlust/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wanderlust.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wanderlust.model.ReviewEntity;
-
-public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
-
-}
